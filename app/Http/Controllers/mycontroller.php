02654a29351ecd306e9beedd9eea8b3d7524da6b
Your Name <you@example.com>
2021-04-21 12:18:03 +0530

    firstcommit

---
+++ app/Http/Controllers/mycontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class mycontroller extends Controller
+{
+    function function1(){
+        
+        echo "my sample";
+        return view('exam');
+    }
+}
---
+++ resources/views/exam.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{url('style.css')}}">
+</head>
+<body>
+<img src="{{url('images/konna.png')}}" alt="">
+<h1>my sample page</h1>
+</body>
+</html>
